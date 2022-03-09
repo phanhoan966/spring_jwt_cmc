42c1b22c14d634a6d0ace3f88e0c6a1c10bf8c36
Hoan9696 <phanhoan.vnu@gmail.com>
2022-03-09 15:56:55 +0700

    Hoan

---
+++ src/main/java/com/example/spring_jwt_cmc/domain/form/RoleToUserForm.java
@@ -0,0 +1,8 @@
+package com.example.spring_jwt_cmc.domain.form;
+import lombok.Data;
+
+@Data
+class RoleToUserForm {
+    private String userName;
+    private String roleName;
+}
--- src/main/java/com/example/spring_jwt_cmc/security/SecurityConfig.java
+++ src/main/java/com/example/spring_jwt_cmc/security/SecurityConfig.java
@@ -34,7 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
         customAuthenticationFilter.setFilterProcessesUrl("/api/login");
-        http.csrf().disable();
+//        http.csrf().disable();
+        http.cors().and().csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh/**").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/users/**")
